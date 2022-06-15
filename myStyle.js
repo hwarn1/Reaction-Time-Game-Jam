02a0244304cfb6c8a94bbf91a589c49dae9e024e
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-06-15 10:43:05 -0400

    Create myStyle.js
    with average and white background for text

---
+++ myStyle.js
@@ -0,0 +1,16 @@
+body {
+  font-family: Verdana, Geneva, sane-serif;
+}
+
+#box {
+  width: 200px;
+  height: 200px;
+  background-color: blue;
+  display: none;
+  position: relative;
+}
+
+.text {
+  background-color: white;
+  color: black;
+}
